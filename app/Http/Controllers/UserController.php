8fa13c7661b9218a36b24f9814aa2cf8994dca7d
Yaroslaw Bagriy <yaroslaw.bagriy@evercode.io>
2021-06-29 07:10:02 -0500

    Finished UserController update method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,15 @@ class UserController extends Controller
     }
 
     public function update(Request $request) {
+        // Validate so values are hex
+        $request->validate([
+            'background_color' => 'required|size:7|starts_with:#',
+            'text_color' => 'required|size:7|starts_with:#',
+        ]);
 
+        Auth::user()->update($request->only(['background_color', 'text_color']));
+
+        return redirect()->back()->with(['success' => 'Changes saved successfully!']);
     }
 
     public function show(User $user) {
